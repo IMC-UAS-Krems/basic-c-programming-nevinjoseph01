f5075fa333789ffe2612b2ec5fcecb4526ff8541
nevin <23imc10614@fh-krems.ac.at>
2024-11-30 15:25:55 +0100

    First commit

--- assignment.c
+++ assignment.c
@@ -11,8 +11,63 @@ int main(int argc, char *argv[]) {
     int minrand = 1;
     int maxrand = 100;
 
-    // WRITE YOUR CODE HERE
     
+    if (argc != 3) {
+        printf("Incorrect usage. You provided %d arguments. The correct number of arguments is 2", argc-1);
+        return 0;
+    }
+
+
+
+    for (int i = 1; i < argc; i++) {
+        if (atoi(argv[i]) <= 0) {
+            printf("Incorrect usage. The parameters you provided are not positive integers");
+            return 0;
+
+        }
+    }
+
+    int row = atoi(argv[1]);
+    int col = atoi(argv[2]);
+
+    int **matrix = (int**)malloc(row*sizeof(int*));
+
+    for (int i = 0; i < row; i++) {
+        matrix[i] = (int*)malloc(col*sizeof(int));
+    }
+
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            matrix[i][j] = minrand + rand() % (maxrand - minrand + 1);
+        }
+    } 
+
+    FILE *pFile = fopen("matrix.txt","w");
+
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            fprintf(pFile, "%d", matrix[i][j]);
+
+            if (j < col -1) {
+                fprintf(pFile, " ");
+            }
+        }
+
+        if (i < row -1) {
+            fprintf(pFile, "\n");
+        }
+        
+    }
+
+    fclose(pFile);
+    
+    for (int i = 0; i < row; i++) {
+        free(matrix[i]);
+    }
+
+    free(matrix);
 
     return 0;
+
+
 }
